1d6f4dfcf973366fea64947e06e07c2b9f94d400
mrTraderOps <143614178+mrTraderOps@users.noreply.github.com>
2024-03-04 23:42:58 +0800

    first commit -organize files -updated css

---
+++ pantryPal/php/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PantryPal</title>
+        <link rel="stylesheet" href="../css/index.css">
+    </head>
+
+    <body>
+        <header>
+
+            <img class="logo" src="../images/PantryPal_Logo_Name.png" alt="PantryPal : Smart Cooking Companion" width="30%">
+   
+            <nav>
+                <ul class="nav_links">
+                    <li><a href="home.html">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact us</a></li>
+                </ul>
+            </nav>
+
+            <a class="loginButton" href="login.php"><button>Login/SignUp</button></a>
+
+        </header>
+        
+        <main>
+            <section class="section1">
+                <h2>PantryPal is an AI bot that suggesting menus, recipes and meals.</h2>
+            </section>
+            <section class="section2">
+                <p>The sole purpose of this project is to help you to think and decide on what you should cook today based on the 
+                    available ingredients you have.
+                </p>
+                <div class="getStartedbutton">
+                    <a href="home.php"><button>Get Started</button></a>
+                </div>
+            </section>
+        </main>
+
+        <footer>
+            <p>&copy;2024 Pantry Pal. All Rights Reserved</p>
+        </footer>
+    </body>
+</html>
